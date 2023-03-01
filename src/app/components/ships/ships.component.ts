3cc8b475d8915392b511290f34d375fc7956d342
Javier-ui <74291836+Javier-ui@users.noreply.github.com>
2023-03-01 13:39:56 +0100

    moved ships services to ship folder
    moved ship services to _services inside of ships folder

--- src/app/components/ships/ships.component.ts
+++ src/app/components/ships/ships.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ShipsService } from 'src/app/services/ships.service';
+import { ShipsService } from './_services/ships.service';
 
 @Component({
   selector: 'app-ships',
@@ -10,7 +10,7 @@ export class ShipsComponent implements OnInit {
 
   public dataList: any = [];
 
-  constructor( private shipsService: ShipsService) {}
+  constructor(private shipsService: ShipsService) { }
 
   ngOnInit(): void {
     this.shipsService.getShips().subscribe((ships) => {
